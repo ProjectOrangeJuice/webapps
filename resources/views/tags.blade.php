373e7f5e963462ee98bc1809f84dfb453e3159e9
oliverharris <githarris@harriso.co.uk>
2019-12-09 17:50:01 +0000

    Format tag return

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -3,12 +3,14 @@
 
 @section("content")
 <h1> {{ $tag->tag }}</h1>
+<hr>
 @if (count($posts) === 0)
     <p>No posts found for this tag!</p>
 @endif
  @foreach ($posts as $post)
-    <p>Title : {{ $post->title }}</p>
-    <p> Content: {{ $post->content }}</p>
+    <h3>{{ $post->title }}</h3>
+    <h5> {{ $post->user }} </h5>
+    <p>{{ Str::limit($post->content,250,"...") }}</p>
     
 @endforeach
 
