21475ed935bc408d0ec521759963552d1e461768
oliverharris <githarris@harriso.co.uk>
2019-12-09 14:22:53 +0000

    Corrected form locations

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,13 +5,13 @@
 <div class="h-75 row align-items-center">
     <div class="col">
         <h1 class="text-center">Some name</h1>
-        <form class="form-group">
+        <form class="form-group" action="/tags" method="GET">
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search for a tag">
                 <button class="btn btn-primary"><i class="fa fa-search"></i></button>
             </div>
         </form>
-        <p class="text-center"><a href="ds">Or pick something random</a></p>
+        <p class="text-center"><a href="/tags">Or pick something random</a></p>
 
     </div>
 </div>
