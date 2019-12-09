b3c1514032c197f66cb60f216b9bf413bd9d94f4
oliverharris <githarris@harriso.co.uk>
2019-12-09 17:10:45 +0000

    No posts message

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -2,8 +2,10 @@
 
 
 @section("content")
-
-{{ $tag }}
+<h1> {{ $tag->tag }}</h1>
+@if (count($posts) === 0)
+    <p>No posts found for this tag!</p>
+@endif
  @foreach ($posts as $post)
     <p>Title : {{ $post->title }}</p>
     <p> Content: {{ $post->content }}</p>
