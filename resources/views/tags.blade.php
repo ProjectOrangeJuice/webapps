9a9a52cbf1b5fe42e76e82c5489657fa1733b1be
oliverharris <githarris@harriso.co.uk>
2019-12-09 17:51:43 +0000

    Changed format

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -8,8 +8,8 @@
     <p>No posts found for this tag!</p>
 @endif
  @foreach ($posts as $post)
-    <h3>{{ $post->title }}</h3>
-    <h5> {{ $post->user }} </h5>
+    <h2>{{ $post->title }}</h2>
+    <h5> By {{ $post->user->name }} </h5>
     <p>{{ Str::limit($post->content,250,"...") }}</p>
     
 @endforeach
