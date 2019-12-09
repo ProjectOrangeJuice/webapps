49ca6f3f4e4e4fc1442f3fc3faf9890f16aff296
oliverharris <githarris@harriso.co.uk>
2019-12-09 14:19:45 +0000

    Format

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,17 +3,17 @@
 
 @section("content")
 <div class="h-75 row align-items-center">
-<div class="col">
-<h1 class="text-center">Some name</h1>
-<form class="form-group">
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search for a tag">
-        <button class="btn btn-primary"><i class="fa fa-search"></i></button>
-    </div>
-</form>
-<p class="text-center"><a href="ds">Or pick something random</a></p>
+    <div class="col">
+        <h1 class="text-center">Some name</h1>
+        <form class="form-group">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for a tag">
+                <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+            </div>
+        </form>
+        <p class="text-center"><a href="ds">Or pick something random</a></p>
 
-</div>
+    </div>
 </div>
 
 
