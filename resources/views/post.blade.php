739e996892a8f4e1ec75d77979366b4b8cf26257
oliverharris <952283@swansea.ac.uk>
2019-12-12 15:57:21 +0000

    layout change

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,22 @@
 @extends("layouts.master")
 
 @section("content")
-<h1>{{ $post->title }} </h1>
 <div class="row">
-<div class="col-sm">
+<div class="col-lg">
+<h1>{{ $post->title }} </h1>
+</div>
+<div class="col">
 <p>Tags: @foreach($post->tags as $tag)
 {{ $loop->first ? '' : "," }}
 {{ $tag->tag }}
 @endforeach
-</p></div>
-<div class="col-sm">
-<p>Author: {{ $post->user->name }}</p></div>
-<div class="col-sm">
-<p>Date: {{ $post->created_at }}</p></div>
+</p>
+<p>Author: {{ $post->user->name }}</p>
+
+<p>Date: {{ $post->created_at }}</p>
 </div>
+</div>
+
 
 <p> {{ $post->content }}</p>
 
