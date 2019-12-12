631035758cdebcdb41f67daf2ff9afd9f20f0db8
oliverharris <952283@swansea.ac.uk>
2019-12-12 20:00:06 +0000

    renamed search to index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends("layouts.master")
+
+
+@section("content")
+<div class="h-75 row align-items-center">
+    <div class="col">
+        <h1 class="text-center">Some name</h1>
+        <form class="form-group" action="/tags" method="GET">
+            <div class="input-group">
+                <input type="text" class="form-control" name="search" placeholder="Search for a tag">
+                <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+            </div>
+        </form>
+        <p class="text-center"><a href="/tags">Or pick something random</a></p>
+        <p class="text-center"><a href="/search">Full search</a></p>
+
+    </div>
+</div>
+
+
+@endsection
