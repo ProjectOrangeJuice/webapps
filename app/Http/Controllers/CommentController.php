f856d99de4982d7f62977e8098bde359ed2a4eab
oliverharris <githarris@harriso.co.uk>
2019-12-15 20:59:08 +0000

    Delete comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Post;
 use App\Comment;
 use Auth;
+
 class CommentController extends Controller
 {
     public function __construct()
     {
-   
+
         $this->middleware('auth');
     }
 
@@ -23,35 +24,22 @@ class CommentController extends Controller
     public function store($post, Request $request)
     {
         //check post exists
-        if(!Post::find($post)){
-            return response('',404);
+        if (!Post::find($post)) {
+            return response('', 404);
         }
 
-        
-            $validatedData = $request->validate([
-                "comment" => "required|min:5|max:1000"
-               ]);
-               $comment = strip_tags($validatedData["comment"]);
-               $newComment = New Comment;
-               $newComment->comment = $comment;
-               $newComment->post_id = $post;
-               $newComment->user_id = Auth::id();
-               $newComment->save();
-               return response($newComment, 200);
-       
-    }
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $post)
-    {
-        //
-    }
 
+        $validatedData = $request->validate([
+            "comment" => "required|min:5|max:1000"
+        ]);
+        $comment = strip_tags($validatedData["comment"]);
+        $newComment = new Comment;
+        $newComment->comment = $comment;
+        $newComment->post_id = $post;
+        $newComment->user_id = Auth::id();
+        $newComment->save();
+        return response($newComment, 200);
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -60,6 +48,15 @@ class CommentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //get the comment
+        $comment = Comment::find($id);
+        if ($comment) {
+            if ($comment->user_id == Auth::id()) {
+                //same person has created it
+                $comment->delete();
+            } else {
+                return response("", 403);
+            }
+        }
     }
 }
