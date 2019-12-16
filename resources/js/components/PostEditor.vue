821066d16e3d3b90972485854f81c80517e64287
oliverharris <githarris@harriso.co.uk>
2019-12-16 13:05:33 +0000

    Tags

--- resources/js/components/PostEditor.vue
+++ resources/js/components/PostEditor.vue
@@ -6,17 +6,32 @@
 
     <div>
         <h3>Title</h3>
-        <input type="text" class="form-control" name="title">
+        <input type="text" class="form-control" name="title" v-model="title">
         <h4>Tags</h4>
         <div class="input-group mb-3">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" v-model="tag">
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button">Add</button>
+                <button class="btn btn-outline-secondary" type="button" @click="addTag">Add</button>
             </div>
         </div>
-        <div>(foreach tag)</div>
+        <div class="row">
+        <div v-for="t in tags" class="col-">
+
+            <div v-if="!t.approved" class="rounded border border-danger">
+            <b>{{ t.name }}</b> <button class="btn btn-danger" @click="removeTag(t)">X</button>
+            </div>
+
+            <div v-else class="rounded border border-success">
+            <b>{{ t.name }}</b> <button class="btn btn-danger">X</button>
+            </div>
+
+        </div>
+
+        </div>
+
+
         <h4>Content</h4>
-        <textarea class="form-control" rows=10></textarea>
+        <textarea class="form-control" rows=10 v-model="content"></textarea>
         <h4>Images</h4>
         <div class="input-group mb-3">
             <input type="file" class="form-control" id="exampleFormControlFile1">
@@ -38,5 +53,26 @@
 
 
 <script>
-export default {}
+export default {
+    data() {
+    return {
+      title: "",
+      tag: "",
+      tags: [],
+      content: "",
+      images: [],
+    };
+  },
+  methods: {
+    addTag: function() {
+        this.tags.push({"name":this.tag,"approved":false});
+        this.tag = "";
+    },
+    removeTag: function($tag) {
+        this.tags.splice($tag,1);
+    }
+  },
+
+
+}
 </script>
