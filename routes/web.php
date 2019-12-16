f1400684b0198365fc6d5b2820f2945a42566478
oliverharris <githarris@harriso.co.uk>
2019-12-16 12:25:49 +0000

    Changed master display

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('index', ["hideSearch"=>true]);
 })->name("home");
 
 Route::get('/tags', "TagController@index")->name("tag.index");
