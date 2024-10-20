b7c4cd519d1cf197cbc9159984adda4692769a67
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:32 +0100

    Gates for post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,35 +9,25 @@ use Auth;
 use File;
 use Illuminate\Support\Facades\Gate;
 use App\Image;
+
 class PostController extends Controller
 {
 
- 
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function perPost($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view("post", ["post" => $post, "edit" => Gate::allows("edit-post", $post)]);
     }
 
-
-    public function perPost($id){
-        $post = Post::find($id);
-        
-        return view("post",["post"=>$post,"edit"=>Gate::allows("edit-post",$post)]);
-    } 
-
-    public function data($id){
-        //$post = Post::with(["tags","images"])->find($id);
-        $post = Post::with(["images","tags" => function ($query) {
-            $query->select('tag',"id","confirmed");
-        }])->where("id",$id)->first();
+    public function data($id)
+    {
+        $post = Post::with(["images", "tags" => function ($query) {
+            $query->select('tag', "id", "confirmed");
+        }])->where("id", $id)->first();
         return $post;
-    } 
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -46,10 +36,10 @@ class PostController extends Controller
      */
     public function create(Request $request)
     {
-        if($request->has("post")){
-        return view("post/create",["post"=> $request->post]);
-        }else{
-            return view("post/create",["post"=> -1]);
+        if ($request->has("post")) {
+            return view("post/create", ["post" => $request->post]);
+        } else {
+            return view("post/create", ["post" => -1]);
         }
     }
 
@@ -63,94 +53,83 @@ class PostController extends Controller
     {
 
         $data = $request->validate([
-            "title"=> 'required|min:5|max:100',
-            "content"=>"required|min:5",
-            "tags"=>"required",
+            "title" => 'required|min:5|max:100',
+            "content" => "required|min:5",
+            "tags" => "required",
         ]);
-        if($request->code == -1){
-        $post = new Post;
-        $post->title = $data["title"];
-        $post->content = $data["content"];
-        $post->user_id = Auth::id();
-        $post->save();
-        foreach($data["tags"] as $tag){
-            $tag = Tag::where("tag",$tag)->first();
-            if($tag != null){
-                $post->tags()->attach($tag);
+        if ($request->code == -1) {
+            $post = new Post;
+            $post->title = $data["title"];
+            $post->content = $data["content"];
+            $post->user_id = Auth::id();
+            $post->save();
+            foreach ($data["tags"] as $tag) {
+                $tag = Tag::where("tag", $tag)->first();
+                if ($tag != null) {
+                    $post->tags()->attach($tag);
+                }
+            }
+            return $post;
+        } else {
+
+            $post = Post::find($request->code);
+            if (Gate::allows("edit-post", $post)) {
+                $post->title = $data["title"];
+                $post->content = $data["content"];
+                $post->save();
+                foreach ($data["tags"] as $tag) {
+                    $tag = Tag::where("tag", $tag)->first();
+                    if ($tag != null) {
+                        $post->tags()->attach($tag);
+                    }
+                }
+
+                return $post;
             }
         }
-        return $post;
-    }else{
-        $post = Post::find($request->code);
-        $post->title = $data["title"];
-        $post->content = $data["content"];
-        $post->save();
-        foreach($data["tags"] as $tag){
-            $tag = Tag::where("tag",$tag)->first();
-            if($tag != null){
-                $post->tags()->attach($tag);
+    }
+
+
+    public function imageDelete($img)
+    {
+
+        $image = Image::where("location", $img);
+        if (Gate::allows("edit-post", $image->post)) {
+            $path = "../public/publicImg/" . $img;
+            if (File::exists($path)) {
+                File::delete($path);
+                $image->delete();
             }
         }
-        return $post;
-
-    }
     }
 
+    public function imageUpload(Request $request)
+    {
+        $post = Post::find($request->post);
+        if (Gate::allows("edit-post", $post)) {
+            request()->validate([
 
-    public function imageDelete($img){
-     
-        $image = Image::where("location",$img);
-        $path = "../public/publicImg/".$img;
-        if(File::exists($path)) {
-            File::delete($path);
-            $image->delete();
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+
+            ]);
+
+
+
+            $imageName = time() . '.' . request()->image->getClientOriginalExtension();
+
+
+
+            request()->image->move(public_path('publicImg'), $imageName);
+
+            $image = new Image;
+            $image->location = $imageName;
+            $image->post_id = $request->post;
+            $image->save();
+
+            return $image;
         }
     }
 
-    public function imageUpload(Request $request){
-        request()->validate([
-
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-
-        ]);
-
-  
-
-        $imageName = time().'.'.request()->image->getClientOriginalExtension();
-
-  
-
-        request()->image->move(public_path('publicImg'), $imageName);
-
-        $image = new Image;
-        $image->location = $imageName;
-        $image->post_id = $request->post;
-        $image->save();
-
-        return $image;
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -161,33 +140,20 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-       
+
         //Update the tags
-        if(Gate::allows("edit-tag",$post)){
+        if (Gate::allows("edit-tag", $post)) {
             $tag = $request->tag;
-            
-          
-            if($request->confirm == "Confirm"){
-                $tagToUpdate =  $post->tags->where("tag",$tag)->first();
+
+
+            if ($request->confirm == "Confirm") {
+                $tagToUpdate =  $post->tags->where("tag", $tag)->first();
                 $tagToUpdate->pivot->confirmed = true;
                 $tagToUpdate->pivot->save();
-            //$post->tags()->sync([$tag->id=>["confirmed"=>true]],false);
-            }else{
+                //$post->tags()->sync([$tag->id=>["confirmed"=>true]],false);
+            } else {
                 $post->tags()->detach($tag->id);
             }
-            
-
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
