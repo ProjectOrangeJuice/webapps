898c4cf8cd62c059c13e93a47a8f682de22529ab
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:31 +0100

    Accounts page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,40 +6,48 @@
 <h1>Your account</h1>
 <div class="row">
 
-<div class="col">
+<div class="col-8">
 <h2>Your posts</h2>
-@foreach ($user->posts as $post)
-    
 
-<div class="row">
-    <div class="col-lg">
-    <h1>{{ $post->title }} </h1>
-    </div>
-    <div class="col">
+@foreach ($user->posts as $post)
+<a href="{{ route('post.show',['id' => $post->id])}}" class="text-body">
+    <div>
+    <h2>{{ $post->title }}</h2>
+    <h5> By {{ $post->user->name }} </h5>
     <p>Tags: @foreach($post->tags as $tag)
-    {{ $loop->first ? '' : "," }}
-    <a href="{{ route('tag.index',['search' => $tag->tag])}}">{{ $tag->tag }}</a>
-    @endforeach
-    </p>
-    <p>Author: {{ $post->user->name }}</p>
-    
-    <p>Date: {{ $post->created_at }}</p>
-    </div>
-    </div>
+{{ $loop->first ? '' : "," }}
+{{ $tag->tag }}
+@endforeach
+</p>
+<p>{{ Str::limit($post->content,250,"...") }}</p>
+</div>
+</a>
+<p class="text-success"> {{ count($post->comments) }} comments
 
 @endforeach
 </div>
 
-<div class="col">
+<div class="col-">
     <form action="POST" method="">
-        Username: {{$user->name}}
+        <div class="form-group">
+            Username: {{$user->name}}
+          </div>
+          <div class="form-group">
         Current password: <input type="password" name="password">
+    </div>
+        <div class="form-group">
         New password: <input type="password" name="np">
+    </div>
+    <div class="form-group">
         Email: <input type="text" name="email" value="{{$user->email}}">
-        Tags you admin:  <p>Tags: @foreach($user->tags as $tag)
+    </div>
+    <div class="form-group">
+        Tags you admin:   @foreach($user->admins as $tag)
             {{ $loop->first ? '' : "," }}
             {{ $tag->tag }}
             @endforeach
+        </div>
+       
     </form>
 </div>
 
