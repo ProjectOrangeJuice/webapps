bc92d8aa65335744f6a9cfdc69b02441fe83a3ef
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:29 +0100

    Tags are shown

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,21 @@
 
 @section("content")
 <h1>{{ $post->title }} </h1>
-<p>Author: {{ $post->user->name }}</p>
+<div class="row">
+<div class="col-sm">
+<p>Tags: @foreach($post->tags as $tag)
+{{ $loop->first ? '' : "," }}
+{{ $tag->tag }}
+@endforeach
+</p></div>
+<div class="col-sm">
+<p>Author: {{ $post->user->name }}</p></div>
+<div class="col-sm">
+<p>Date: {{ $post->created_at }}</p></div>
+</div>
+
 <p> {{ $post->content }}</p>
+
+<hr>
+<h2>Comments</h2>
 @endsection
