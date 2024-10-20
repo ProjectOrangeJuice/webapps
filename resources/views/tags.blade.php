c6afe221790f851bb4c7cd92dc705bcbf95d4a61
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:29 +0100

    Display errors

--- resources/views/tags.blade.php
+++ resources/views/tags.blade.php
@@ -2,17 +2,26 @@
 
 
 @section("content")
-<h1> {{ $tag->tag }}</h1>
+<h1> {{ $tag->tag ?? "Oops!" }}</h1>
 <hr>
-@if (count($posts) === 0)
-    <p>No posts found for this tag!</p>
-@endif
- @foreach ($posts as $post)
-    <h2>{{ $post->title }}</h2>
-    <h5> By {{ $post->user->name }} </h5>
-    <p>{{ Str::limit($post->content,250,"...") }}</p>
+@if (count($posts ?? []) === 0)
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li> {{ $error }}
+            @endforeach
+        </div>
+    @else
+        <p>No posts found for this tag!</p>
+    @endif
     
-@endforeach
-
+@else
+    @foreach ($posts as $post)
+        <h2>{{ $post->title }}</h2>
+        <h5> By {{ $post->user->name }} </h5>
+        <p>{{ Str::limit($post->content,250,"...") }}</p>
+        
+    @endforeach
+@endif
 
 @endsection
