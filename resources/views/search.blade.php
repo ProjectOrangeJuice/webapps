750127529a0c9c007bbc9a40a208990a1a519aef
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:28 +0100

    Created form for search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,5 +2,19 @@
 
 
 @section("content")
-Hello world
+<div class="h-75 row align-items-center">
+<div class="col">
+<h1 class="text-center">Some name</h1>
+<form class="form-group">
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for a tag">
+        <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+    </div>
+</form>
+<p class="text-center"><a href="ds">Or pick something random</a></p>
+
+</div>
+</div>
+
+
 @endsection
