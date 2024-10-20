3a52a1c0e59578a5385d0093ddf3579926326284
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:47:29 +0100

    Admin user account

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -11,8 +10,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $notepad = factory(App\Notepad::class)->make();
+        $notepad->user->admin = true;
+        $notepad->save();
+
         //factory(App\User::class,50)->create();
         //Create a notepad and user at the same time
-        factory(App\Notepad::class,50)->create();
+        factory(App\Notepad::class,49)->create();
     }
 }
+
